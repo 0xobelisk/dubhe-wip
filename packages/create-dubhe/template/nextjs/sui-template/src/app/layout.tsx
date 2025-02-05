3492e7762b054b556421617a9c3671b29e4989b3
vladilen11 <vladilenaksana@163.com>
2025-02-05 17:49:12 +0800

    feat(create-dubhe): upgrade to v0.1.2, update sui template code

---
+++ packages/create-dubhe/template/nextjs/sui-template/src/app/layout.tsx
@@ -0,0 +1,17 @@
+import './globals.css';
+import '@mysten/dapp-kit/dist/index.css';
+import { Providers } from './providers';
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className="antialiased" suppressHydrationWarning>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}
--- packages/create-dubhe/template/nextjs/sui-template/src/chain/config.ts
+++ packages/create-dubhe/template/nextjs/sui-template/src/chain/config.ts
@@ -1,5 +1,5 @@
 type NetworkType = 'testnet' | 'mainnet' | 'devnet' | 'localnet';
 
-export const NETWORK: NetworkType = 'testnet';
-export const PACKAGE_ID = '0xecf70ffbdc1af4d2b781aec4f5cbb5558c7c559c5f9e52ef4da770d3c2f3ad0d'
-export const SCHEMA_ID = '0x1117240e366ca68db5b0841564313bfba957c6eca365772786d63f4d734ec77f'
+export const NETWORK: NetworkType = 'localnet';
+export const PACKAGE_ID = '0x6c0d1a23aefeb10bb53837dd23463420dd7c4edce6f762882ae75ea16cbd702c'
+export const SCHEMA_ID = '0xe60a62fb4c195d3867b33ca55d536c84373c66caa603c9ace0013a5a1a86de64'
