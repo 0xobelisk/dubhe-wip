e7d2149a5d97e08e6653e61c2e83b514d7c87ee8
web3olalala <web3bob.dev@gmail.com>
2025-03-28 21:43:26 +0800

    [examples]: add dms

---
+++ examples/sui/dms/dubhe.config.ts
@@ -0,0 +1,18 @@
+import { DubheConfig, storage } from "@0xobelisk/sui-common";
+
+export const dubheConfig = {
+  name: "dms",
+  description: "Distributed Messaging",
+  schemas: {
+    message: "StorageValue<String>",
+  },
+  errors: {
+    invalid_content_length: "Content length must be less than 12",
+  },
+  events: {
+    message_sent: {
+      sender: "address",
+      content: "String",
+    },
+  },
+} as DubheConfig;
