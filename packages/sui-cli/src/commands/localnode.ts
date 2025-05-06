2d4f9391b9051cb442225fc5d754960c0cfeb037
web3olalala <web3bob.dev@gmail.com>
2025-05-06 23:06:14 +0800

    [sui-cli] update start node

--- packages/sui-cli/src/commands/localnode.ts
+++ packages/sui-cli/src/commands/localnode.ts
@@ -8,17 +8,11 @@ const commandModule: CommandModule = {
 
   builder(yargs) {
     return yargs
-      .option('force-regenesis', {
-        alias: 'f',
-        type: 'boolean',
-        description: 'Force regenesis the local node',
-        default: true
-      });
   },
 
-  async handler(argv) {
+  async handler() {
     try {
-      await startLocalNode({ forceRegenesis: argv['force-regenesis'] as boolean });
+      await startLocalNode();
     } catch (error) {
       console.error('Error executing command:', error);
       process.exit(1);
