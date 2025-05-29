f4e80bea31fd200aa741e8f9d86fb13d1c999353
vladilen11 <vladilenaksana@163.com>
2025-05-29 08:35:43 +0800

    feat(sui-cli): fix version and error log

--- packages/sui-cli/src/dubhe.ts
+++ packages/sui-cli/src/dubhe.ts
@@ -2,12 +2,13 @@
 
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
+import chalk from 'chalk';
 import { commands } from './commands';
 import { logError } from './utils/errors';
+import packageJson from '../package.json';
 
 // Load .env file into process.env
 import * as dotenv from 'dotenv';
-import chalk from 'chalk';
 dotenv.config();
 
 yargs(hideBin(process.argv))
@@ -16,6 +17,9 @@ yargs(hideBin(process.argv))
   // Use the commands directory to scaffold
   // eslint-disable-next-line @typescript-eslint/no-explicit-any -- command array overload isn't typed, see https://github.com/yargs/yargs/blob/main/docs/advanced.md#esm-hierarchy
   .command(commands as any)
+  .version(packageJson.version)
+  .demandCommand(1, 'Please provide a command')
+  .recommendCommands()
   // Enable strict mode.
   .strict()
   // Custom error handler
@@ -29,8 +33,11 @@ yargs(hideBin(process.argv))
       );
     }
     console.log('');
-    logError(err);
-    console.log('');
+    // Even though `.fail` type says we should get an `Error`, this can sometimes be undefined
+    if (err != null) {
+      logError(err);
+      console.log('');
+    }
 
     process.exit(1);
   })
