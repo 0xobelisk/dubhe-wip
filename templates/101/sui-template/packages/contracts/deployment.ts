4f8abdf17665ea0303fed8f0ac654e098e8cde2f
Vladilen <125375292+vladilen11@users.noreply.github.com>
2025-07-20 19:53:21 +0800

    feat: update e2e test folder name and template start cmd (#189)

--- templates/101/sui-template/packages/contracts/deployment.ts
+++ templates/101/sui-template/packages/contracts/deployment.ts
@@ -1,5 +1,5 @@
 type NetworkType = 'testnet' | 'mainnet' | 'devnet' | 'localnet';
 
 export const NETWORK: NetworkType = 'localnet';
-export const PACKAGE_ID = '0xc151eaccdb11372cb19fbd087b9e61b9047e8f0f83272ccb4f30c6bc8395e49f';
-export const DUBHE_SCHEMA_ID = '0x5c8d7dfed2e59bd0e744ff3a23fdee059c5c86c097e62de4a0b41b4afe6cdf99';
+export const PACKAGE_ID = '0x0ca6c2ae2524851fa389c3213435a0c835d08e21c4ecd9c08decfd3d5635f45c';
+export const DUBHE_SCHEMA_ID = '0xf6ba619c2f5aab287194c6c57c82b20686333b5422d350943972a1cedb1e0f40';
