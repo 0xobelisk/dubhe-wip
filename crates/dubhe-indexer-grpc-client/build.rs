81c3510a31f2a02b2560003c21521d885c2370f9
henryliu <17600047539@163.com>
2026-03-21 11:58:25 +0800

    fix: include protobuf well-known types in dokploy build

--- crates/dubhe-indexer-grpc-client/build.rs
+++ crates/dubhe-indexer-grpc-client/build.rs
@@ -1,11 +1,31 @@
+use std::path::{Path, PathBuf};
+
+fn protobuf_include_dirs(proto_dir: &str) -> Vec<PathBuf> {
+    let mut includes = vec![PathBuf::from(proto_dir)];
+
+    if let Ok(include) = std::env::var("PROTOC_INCLUDE") {
+        includes.push(PathBuf::from(include));
+    }
+
+    for candidate in ["/usr/include", "/usr/local/include", "/opt/homebrew/include"] {
+        let path = Path::new(candidate).join("google/protobuf/struct.proto");
+        if path.exists() {
+            includes.push(PathBuf::from(candidate));
+        }
+    }
+
+    includes
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Copy proto files from dubhe-indexer-grpc
     let proto_dir = "../dubhe-indexer-grpc/proto";
-    let proto_file = "dubhe_grpc.proto";
+    let proto_file = format!("{proto_dir}/dubhe_grpc.proto");
+    let includes = protobuf_include_dirs(proto_dir);
+    let include_refs = includes.iter().map(PathBuf::as_path).collect::<Vec<_>>();
 
     tonic_build::configure()
-        .build_server(false) // We only need client
-        .compile(&[&format!("{}/{}", proto_dir, proto_file)], &[proto_dir])?;
+        .build_server(false)
+        .compile(&[proto_file.as_str()], &include_refs)?;
 
     Ok(())
 }
--- crates/dubhe-indexer-grpc/build.rs
+++ crates/dubhe-indexer-grpc/build.rs
@@ -1,4 +1,26 @@
+use std::path::{Path, PathBuf};
+
+fn protobuf_include_dirs() -> Vec<PathBuf> {
+    let mut includes = vec![PathBuf::from("proto")];
+
+    if let Ok(include) = std::env::var("PROTOC_INCLUDE") {
+        includes.push(PathBuf::from(include));
+    }
+
+    for candidate in ["/usr/include", "/usr/local/include", "/opt/homebrew/include"] {
+        let path = Path::new(candidate).join("google/protobuf/struct.proto");
+        if path.exists() {
+            includes.push(PathBuf::from(candidate));
+        }
+    }
+
+    includes
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/dubhe_grpc.proto")?;
+    let includes = protobuf_include_dirs();
+    let include_refs = includes.iter().map(PathBuf::as_path).collect::<Vec<_>>();
+
+    tonic_build::configure().compile(&["proto/dubhe_grpc.proto"], &include_refs)?;
     Ok(())
 }
